4a56f04b4edf88b575f5fae9bc2b1033a8c25085
Ali Hammad <hey@alihammad.net>
2026-04-07 14:15:03 +0300

    refactor: remove unused imports and simplify BottomNavigation event handler

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -45,7 +45,7 @@ export default function BottomNav() {
     <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 1000 }}>
       <BottomNavigation
         value={getCurrentIndex()}
-        onChange={(event, newValue) => {
+        onChange={(_, newValue) => {
           navigate(mobileNavItems[newValue].path)
         }}
         sx={{
